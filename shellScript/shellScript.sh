feb8f930128cdb2e281a5836d38e2facb99a9940
Mayank <smayank748@gmail.com>
2022-10-03 09:16:29 +0530

    [Mayank]working variable initialization

---
+++ shellScript/shellScript.sh
@@ -0,0 +1,12 @@
+#! /bin/bash -x
+echo "Hello World"
+isBool=3
+echo $isBool
+
+#Example
+commitCount=0
+commitId=''
+read -p "Enter the commit id: " commitId
+commitCount=`git log | grep -i commit | grep -v '\[' | awk -v var=$commitId '$2==var {print NR}'`
+echo $commitCount
+git rebase -i HEAD~$commitCount
