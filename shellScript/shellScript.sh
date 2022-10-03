1fc9891dc0b20fdd01edf668b02cdef2a9a63e72
Mayank <smayank748@gmail.com>
2022-10-03 09:35:04 +0530

    [Mayank]operator's introduction or basic operation

--- shellScript/shellScript.sh
+++ shellScript/shellScript.sh
@@ -3,3 +3,8 @@ echo "Hello World"
 isBool=3
 echo $isBool
 
+#Operator's
+#1.Arit operat(+,-,/,*,%,++,--)
+#2.Relation oper(-eq,-lt,-le,-gt,-ge,==,!=,<=,>=)
+#3.Boolean oper(|| -o -a &&)
+#4.File oper()
