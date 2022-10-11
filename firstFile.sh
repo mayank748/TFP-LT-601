d8bc94174288d50351409c077ca535991f1dd6bd
Mayank <smayank748@gmail.com>
2022-10-11 09:07:33 +0530

    [Mayank]worked on dictionary

--- firstFile.sh
+++ firstFile.sh
@@ -1,5 +1,31 @@
-echo "Hello world..."
-echo "Made from bash"
-echo "change made by fellow developer"
-echo "to take pull change"
-echo "remove the line message"
+#! /bin/bash -x
+
+declare -A myDictionary
+
+myDictionary["Bikes"]="Bobber"
+myDictionary["Car"]="Sedan"
+myDictionary["Bus"]="HSRTC"
+
+echo "Before adding value"
+echo ${myDictionary[@]}
+echo ${myDictionary["Bikes"]}
+echo ${!myDictionary[@]}
+
+i=true
+while $i
+do
+	read -p "Enter the key for dicitonary " key
+	read -p "Enter the value for the respective key " value
+	myDictionary[$key]=$value
+	read -p "Do you want to continue(Press y for yes and n for no) " yesNo
+	yesNo="${yesNo,,}"
+	if [ $yesNo == n ]
+	then
+		i=false
+	fi
+done
+
+echo "After adding value"
+echo ${myDictionary[@]}
+echo ${myDictionary["Bikes"]}
+echo ${!myDictionary[@]}
